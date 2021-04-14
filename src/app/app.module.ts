28d7f16c1cd8ead4eea9bc7d110e5e7ed5cba41c
ang-sant-abril <ang-sant-abril@gmail.com>
2021-04-14 08:02:22 +0000

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListadoProveedoresComponent } from './proveedores/listado-proveedores/listado-proveedores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoProveedoresComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/servicios/proveedores.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProveedoresService {
+
+  proveedores: any = [
+    {nombre: 'Iberdrola', cif: 'A12345678'},
+    {nombre: 'Gas Natural', cif: 'A87654321'},
+    {nombre: 'Vodafone', cif: 'A6789543'}
+  ];
+
+  constructor() { }
+
+  getProveedores(): any {
+    return this.proveedores;
+  }
+}
